eff5b5c4de67148c105f04c9b96a7f9f5d9fb154
FlorianLehmann <Lehmann.florian@yandex.com>
2019-02-08 09:56:13 +0100

    cut video into multiple frames in order to call azure API

---
+++ backend/video/estimation/estimate_video.go
@@ -0,0 +1,30 @@
+package estimation
+
+import (
+	"log"
+
+	"gocv.io/x/gocv"
+)
+
+func Estimate(uuid string) {
+
+	img := gocv.NewMat()
+
+	video, err := gocv.VideoCaptureFile("/tmp/" + uuid)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		if !video.IsOpened() {
+			video.Close()
+			break
+		}
+		video.Read(&img)
+
+		// call azure api
+
+	}
+
+}
